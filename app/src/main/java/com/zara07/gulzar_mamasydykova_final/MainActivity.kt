015ad0e7c0f27e568dbf20e846d9b2512a1f07c0
STRIX in Notnik_kg <mamasydykova_g@auca.kg>
2021-12-13 13:23:19 +0600

    first

---
+++ app/src/main/java/com/zara07/gulzar_mamasydykova_final/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.zara07.gulzar_mamasydykova_final
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
